68b8ed7aec5b550f3d54a389d6e2a0c3e82a555a
AvalosJorge <xjorgeluis93x@gmail.com>
2022-07-12 18:51:06 -0300

    creacion de header con logos

--- ProyectoIntegrador_AvalosJorge/src/app/app.module.ts
+++ ProyectoIntegrador_AvalosJorge/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { FondoComponent } from './components/fondo/fondo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    FondoComponent
   ],
   imports: [
     BrowserModule,
